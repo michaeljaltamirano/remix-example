8ae43043a1402a8dfbac7f31e7718595b5eea25c
michaeljaltamirano <michaeljaltamirano@gmail.com>
2022-03-25 16:48:53 -0700

    Next steps up to prisma

---
+++ blog-tutorial/app/models/post.server.ts
@@ -0,0 +1,17 @@
+type Post = {
+  slug: string;
+  title: string;
+};
+
+export async function getPosts(): Promise<Post[]> {
+  return Promise.resolve([
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ]);
+}
---
+++ blog-tutorial/app/routes/posts/index.tsx
@@ -0,0 +1,31 @@
+import { Link, json, useLoaderData } from "remix";
+import { getPosts } from "~/models/post.server";
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: await getPosts(),
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData<LoaderData>();
+  console.log("posts", posts);
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
