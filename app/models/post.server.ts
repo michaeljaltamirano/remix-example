f83303eb739eda85f48ef661482e0c4e0fa54b31
michaeljaltamirano <michaeljaltamirano@gmail.com>
2022-03-25 23:47:22 -0700

    Commit end of tutorial WIP

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,18 @@
-interface Post {
-  slug: string;
-  title: string;
+import type { Post } from "@prisma/client";
+import { prisma } from "~/db.server";
+
+export type { Post } from "@prisma/client";
+
+export async function getPosts() {
+  return prisma.post.findMany();
 }
 
-export async function getPosts(): Promise<Post[]> {
-  return Promise.resolve([
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ]);
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
 }
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -26,6 +26,9 @@ export default function Posts() {
           </li>
         ))}
       </ul>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
     </main>
   );
 }
